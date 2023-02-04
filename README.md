039c6bf114bf3e246ddb4ebbdbb067f9dfe441ab
jerry <calljerryli@outlook.com>
2023-02-05 03:28:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Osmos
+
+Rust + WASM + 神经网络 + 遗传算法
+
+Bilibili 视频：
+
+## LICENSE
+
+[GNU Affero General Public License v3.0](https://choosealicense.com/licenses/agpl-3.0)
---
+++ osmos-nn/src/neuron.rs
@@ -0,0 +1,48 @@
+pub struct Neuron {
+    bias: f32,
+    weight_list: Vec<f32>,
+}
+
+impl Neuron {
+    pub fn new(bias: f32, weight_list: Vec<f32>) -> Self {
+        Self { bias, weight_list }
+    }
+
+    pub fn random(weight_list_size: usize) -> Self {
+        let mut rand = rand::thread_rng();
+        let bias = rand::Rng::gen_range(&mut rand, -1.0..=1.0);
+        let weight_list = (0..weight_list_size)
+            .map(|_| rand::Rng::gen_range(&mut rand, -1.0..=1.0))
+            .collect::<Vec<f32>>();
+        Self { bias, weight_list }
+    }
+
+    pub fn forward(&self, input_list: Vec<f32>) -> f32 {
+        let sum = input_list
+            .iter()
+            .zip(&self.weight_list)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+        sum + self.bias
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod random {
+        #[test]
+        fn test() {
+            let neuron = crate::neuron::Neuron::random(100);
+            assert!(neuron.weight_list.len() == 100)
+        }
+    }
+
+    mod forward {
+        #[test]
+        fn test() {
+            let neuron = crate::neuron::Neuron::new(1.0, vec![2.0, 3.0, 4.0]);
+            let output = neuron.forward(vec![2.0, 2.0, 2.0]);
+            assert!(output == 19.0)
+        }
+    }
+}
