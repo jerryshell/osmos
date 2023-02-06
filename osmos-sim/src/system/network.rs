8554419871e10889facc7be00159728e493c4fc9
jerry <calljerryli@outlook.com>
2023-02-06 21:03:18 +0800

    update network::process()

--- osmos-sim/src/system/network.rs
+++ osmos-sim/src/system/network.rs
@@ -1,13 +1,17 @@
 pub fn process(object_list: &mut [crate::object::Object]) {
     for object in object_list {
         let nn_output = object.network.feed(&object.cell.sensor.data_list);
-        object.network_output = nn_output.clone();
+
         let up = nn_output[0];
         let right = nn_output[1];
         let down = nn_output[2];
         let left = nn_output[3];
-        object.cell.acceleration =
-            nalgebra::Vector2::new((right - left) / 1000.0, (down - up) / 1000.0);
+
+        let acc_x = right - left;
+        let acc_y = down - up;
+        object.cell.acceleration = nalgebra::Vector2::new(acc_x, acc_y);
+
+        object.network_output = nn_output;
     }
 }
 
