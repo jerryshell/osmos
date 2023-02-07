bd1f25f5926b847b350a95b423b666d52296fa38
jerry <calljerryli@outlook.com>
2023-02-08 01:45:21 +0800

    clean redundant code

--- osmos-ga/src/lib.rs
+++ osmos-ga/src/lib.rs
@@ -8,7 +8,6 @@ pub fn selection(rng: &mut rand::rngs::ThreadRng, object_list: &[impl Fitness]) 
         let random_index = rand::Rng::gen_range(rng, 0..object_list.len());
         let fitness = object_list[random_index].fitness();
         let probability = fitness as f64 / total_fitness as f64;
-        println!("{probability}");
         let win = rand::Rng::gen_bool(rng, probability);
         if win {
             return random_index;
